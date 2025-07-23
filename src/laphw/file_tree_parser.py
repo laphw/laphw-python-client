28292e47daefb0af5645403384007c4c6090cbdf
Kenneth Karlsson <24266371+karlssonkennneth@users.noreply.github.com>
2025-07-23 16:16:55 +0200

    refactor: major refactor of file_tree_parser
      WORKING:
      - get_distributions() - returns frozenset of Distribution objects
      - Basic brand parsing from folder structure
    
      BROKEN/TODO:
      - get_models() commented out - relationship logic needs fixing
      - Need to refactor common parsing logic
      - Code quality cleanup needed
    
      Next: Fix brand->model relationships and uncomment tests"

--- src/laphw/file_tree_parser.py
+++ src/laphw/file_tree_parser.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
+from functools import lru_cache
 from pathlib import Path
 from pprint import pprint
 from typing import Any
 
 import frontmatter
-
-Filename = str
 
 
 def get_fixes_folder() -> Path:
@@ -19,41 +18,67 @@ FIXES_ROOT_FOLDER = get_fixes_folder()
 
 
 @dataclass(frozen=True, kw_only=True)
-class MetaData:
-    distributions: set[str]
-    brands: set[str]
-    models: set[str]
-    tags: list[str]
-    difficulty: str
-    tested_by: set[str]
+class Distribution:
+    """Support by each distribution"""
+
+    distribution_name: str
+    distribution_path: Path
+    common_path: Path
+    supported_brands: frozenset[str]
+    supported_models: frozenset[str]
+    fixes: frozenset[str]
 
 
 @dataclass(frozen=True, kw_only=True)
-class Distributions:
-    distributions_names: frozenset[str]
-    distribution_paths: frozenset[Path]
-    common_folder_path: frozenset[Path]
+class Brand:
+    """Support by each brand"""
 
-
-@dataclass(frozen=True, kw_only=True)
-class Brands:
-    brand_names: frozenset[str]
+    brand_name: str
     brand_paths: frozenset[Path]
-    common_folder_paths: frozenset[Path]
+    common_paths: frozenset[Path]
+    supported_distributions: frozenset[str]
+    supported_models: frozenset[str]
+    fixes: frozenset[str]
 
 
 @dataclass(frozen=True, kw_only=True)
-class Models:
-    model_names: frozenset[str]
-    model_paths: frozenset[Path]
-    common_folder_paths: frozenset[Path]
+class Model:
+    """Support by each model"""
+
+    model_name: str
+    model_path_documents: frozenset[Path]
+    common_path_documents: frozenset[Path]
+    brand: str
+    distribution_names: frozenset[str]
 
 
 @dataclass(frozen=True, kw_only=True)
-class Fixes:
-    fix_file_names: frozenset[str]
-    fix_file_paths: frozenset[Path]
-    fix_file_stem: frozenset[str]
+class Fix:
+    """Each fix document mapped with dist, model and brand"""
+
+    file_name: str
+    file_path: Path
+    distribution: str  # Ubuntu, arch etc
+    brand: str  # Dell, Framework etc
+    model: str  # xps-13-9370, framework-laptop-13 etc
+    hw: str  # audio, bluetooth etc
+
+
+@dataclass(frozen=True, kw_only=True)
+class Laphw:
+    """Folder structure representation"""
+
+    distributions: frozenset[Distribution]
+    brands: frozenset[Brand]
+    models: frozenset[Model]
+    fixes: frozenset[Fix]
+
+
+def create_datastructure() -> frozenset[Distribution]:  # should return Laphw
+    distributions_data = get_distributions()
+    # brands_data = get_brands(distributions_data)
+
+    return distributions_data
 
 
 def check_dir(path: Path) -> None:
@@ -75,74 +100,195 @@ def get_sub_dir_paths(parent: Path) -> list[Path]:
     return [dir for dir in parent.iterdir() if dir.is_dir()]
 
 
-def get_distributions() -> Distributions:
+def get_supported_distributions() -> list[str]:
     distribution_paths = [dist for dist in FIXES_ROOT_FOLDER.iterdir() if dist.is_dir()]
-    distribution_names = [
-        path.name for path in distribution_paths if path.name != "common"
-    ]
-    common_folders = [path for path in distribution_paths if path.name == "common"]
-    distribution_paths = [
-        path for path in distribution_paths if "common" not in path.name
-    ]
-
-    return Distributions(
-        distributions_names=frozenset(distribution_names),
-        distribution_paths=frozenset(distribution_paths),
-        common_folder_path=frozenset(common_folders),
-    )
+    return [path.name for path in distribution_paths if path.name != "common"]
 
 
-def get_brands() -> Brands:
-    brand_names = []
-    brand_paths = []
-    distribution_paths = get_distributions()
-    for distribution_path in distribution_paths.distribution_paths:
-        brand_names.extend(get_sub_dir_names(distribution_path))
+@lru_cache(maxsize=1)
+def get_distributions() -> frozenset[Distribution]:
+    distributions = get_supported_distributions()
+    distribution_data = []
+    for distribution in distributions:
+        supported_brands = []
+        brand_paths = []
+        supported_models = []
+        distribution_path = FIXES_ROOT_FOLDER / distribution
+        common_path = distribution_path / "common"
+        supported_brands.extend(get_sub_dir_names(distribution_path))
         brand_paths.extend(get_sub_dir_paths(distribution_path))
 
-    common_folder_paths = [path for path in brand_paths if "common" in path.name]
+        for brand_path in brand_paths:
+            supported_models.extend(get_sub_dir_names(brand_path))
 
-    brand_paths = [path for path in brand_paths if "common" not in path.name]
+        fix_file_paths = list(Path(distribution_path).rglob("*.md"))
+        fix_file_stem = [hardware_fix.stem for hardware_fix in fix_file_paths]
 
-    return Brands(
-        brand_names=frozenset(brand_names),
-        brand_paths=frozenset(brand_paths),
-        common_folder_paths=frozenset(common_folder_paths),
-    )
+        distribution_data.append(
+            Distribution(
+                distribution_name=distribution,
+                distribution_path=distribution_path,
+                common_path=common_path,
+                supported_brands=frozenset(supported_brands),
+                supported_models=frozenset(supported_models),
+                fixes=frozenset(fix_file_stem),
+            )
+        )
 
-
-def get_models() -> Models:
-    model_names = []
-    model_paths = []
-    fixed_brands = get_brands()
-    for brand_path in fixed_brands.brand_paths:
-        model_names.extend(get_sub_dir_names(brand_path))
-        model_paths.extend(get_sub_dir_paths(brand_path))
-
-    common_folder_paths = [
-        model_path for model_path in model_paths if "common" in model_path.name
-    ]
-
-    model_paths = [
-        model_path for model_path in model_paths if "common" not in model_path.name
-    ]
-
-    return Models(
-        model_names=frozenset(model_names),
-        model_paths=frozenset(model_paths),
-        common_folder_paths=frozenset(common_folder_paths),
-    )
+    return frozenset(distribution_data)
 
 
-def get_fixes_documents() -> Fixes:
+def get_all_supported_brands(
+    distribution_data: frozenset[Distribution],
+) -> frozenset[str]:
+    all_supported_brands: frozenset[str] = frozenset()
+    for dist in distribution_data:
+        all_supported_brands = all_supported_brands.union(dist.supported_brands)
+
+    return all_supported_brands
+
+
+def get_all_supported_distributions(
+    distribution_data: frozenset[Distribution],
+) -> frozenset[str]:
+    all_supported_distributions: frozenset[str] = frozenset()
+
+    for dist in distribution_data:
+        all_supported_distributions = all_supported_distributions.union(
+            dist.distribution_name
+        )
+
+    return all_supported_distributions
+
+
+def get_all_supported_models(
+    distribution_data: frozenset[Distribution],
+) -> frozenset[str]:
+    all_supported_models: frozenset[str] = frozenset()
+
+    for dist in distribution_data:
+        all_supported_models = all_supported_models.union(dist.supported_models)
+
+    return all_supported_models
+
+
+@lru_cache(maxsize=1)
+def get_brands(distribution_data: frozenset[Distribution]) -> frozenset[Brand]:
+    all_supported_brands = get_all_supported_brands(distribution_data)
+    brand_data = []
+    for brand in all_supported_brands:
+        brand_paths = []
+        common_paths = []
+        supported_distributions = []
+        supported_models = []
+        fixes: list[str] = []
+        for dist in distribution_data:
+            distribution_path = FIXES_ROOT_FOLDER / dist.distribution_name
+            if brand in dist.supported_brands:
+                supported_distributions.append(dist.distribution_name)
+
+            brand_paths.extend(get_sub_dir_paths(distribution_path))
+            brand_paths.extend(
+                [brand_path for brand_path in brand_paths if brand in brand_path.name]
+            )
+
+            for brand_path in brand_paths:
+                common_paths.extend(get_sub_dir_paths(brand_path))
+                supported_models.extend(get_sub_dir_names(brand_path))
+            common_paths.extend(
+                [
+                    brand_path
+                    for brand_path in common_paths
+                    if "common" in brand_path.name
+                ]
+            )
+            supported_models.extend(
+                [brand_path for brand_path in supported_models if brand in brand_path]
+            )
+
+        brand_data.append(
+            Brand(
+                brand_name=brand,
+                brand_paths=frozenset(brand_paths),
+                common_paths=frozenset(common_paths),
+                supported_distributions=frozenset(supported_distributions),
+                supported_models=frozenset(supported_models),
+                fixes=frozenset(fixes),
+            )
+        )
+
+    return frozenset(brand_data)
+
+
+#
+# @lru_cache(maxsize=1)
+# def get_models() -> model:
+#     model_names = []
+#     model_paths = []
+#     fixed_brands = get_brands()
+#     for brand_path in fixed_brands.brand_paths:
+#         model_names.extend(get_sub_dir_names(brand_path))
+#         model_paths.extend(get_sub_dir_paths(brand_path))
+#
+#     common_folder_paths = [
+#         model_path for model_path in model_paths if "common" in model_path.name
+#     ]
+#
+#     model_paths = [
+#         model_path for model_path in model_paths if "common" not in model_path.name
+#     ]
+#
+#     return model(
+#         model_names=frozenset(model_names),
+#         model_paths=frozenset(model_paths),
+#         common_folder_paths=frozenset(common_folder_paths),
+#     )
+#
+#
+# def get_specific_model(model: str) -> models:
+#     all_model_paths = get_models()
+#     pprint(all_model_paths)
+#     common_path_for_model = [
+#         path for path in all_model_paths.common_folder_paths if model in path.name
+#     ]
+#     model_paths = [path for path in all_model_paths.model_paths if model in path.name]
+#
+#     return models(
+#         model_names=frozenset([model]),
+#         model_paths=frozenset(model_paths),
+#         common_folder_paths=frozenset(common_path_for_model),
+#     )
+#
+#
+# def get_specific_brand(brand: str) -> brands:
+#     all_brand_paths = get_brands()
+#     common_path_for_model = [
+#         path for path in all_brand_paths.common_folder_paths if brand in path.name
+#     ]
+#     brand_paths = [path for path in all_brand_paths.brand_paths if brand in path.name]
+#
+#     return brands(
+#         brand_names=frozenset([brand]),
+#         brand_paths=frozenset(brand_paths),
+#         common_folder_paths=frozenset(common_path_for_model),
+#     )
+#
+#
+@lru_cache(maxsize=1)
+def get_fixes_documents() -> Fix:
+    # TODO Need major refactor
     fix_file_paths = list(Path(FIXES_ROOT_FOLDER).rglob("*.md"))
-    fix_file_names = [file_path.name for file_path in fix_file_paths]
-    fix_file_stem = [hardware_fix.stem for hardware_fix in fix_file_paths]
+    #  fix_file_paths = list(path(fixes_root_folder).rglob("*.md"))
+    # fix_file_names = [file_path.name for file_path in fix_file_paths]
+    # fix_file_stem = [hardware_fix.stem for hardware_fix in fix_file_paths]
 
-    return Fixes(
-        fix_file_names=frozenset(fix_file_names),
-        fix_file_paths=frozenset(fix_file_paths),
-        fix_file_stem=frozenset(fix_file_stem),
+    return Fix(
+        file_name="file_name",
+        file_path=fix_file_paths[0],
+        distribution="distribution",
+        brand="Dell",
+        model="framework-laptop-13",
+        hw="audio",
     )
 
 
@@ -196,8 +342,21 @@ def get_brand_model_distribution(search_string: str, fixes: set[Path]) -> set[Pa
     return set(found_fixes)
 
 
+def clear_all_cache() -> None:
+    get_distributions.cache_clear()
+    # get_brands.cache_clear()
+    # get_models.cache_clear()
+    # get_fixes_documents.cache_clear()
+
+
 if __name__ == "__main__":
-    pprint(get_distributions())
-    pprint(get_brands())
-    pprint(get_models())
+    clear_all_cache()
+    # pprint(get_model_support_for_distr())
+    test = create_datastructure()
+    # pprint(test)
+    # pprint(get_specific_brand("dell"))
+    # pprint(get_distributions())
+
+    pprint(get_brands(get_distributions()))
+    # pprint(get_models())
     # pprint(get_brand_model_distribution("ubuntu"))
